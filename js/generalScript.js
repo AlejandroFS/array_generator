b824f07318ca6f813c16ca28ad6d3eb1799427b4
Jorge Frias <death_note59@hotmail.com>
2019-10-18 18:28:09 -0500

    1 dimensional row

--- js/generalScript.js
+++ js/generalScript.js
@@ -2,30 +2,69 @@ let button;
 document.addEventListener("DOMContentLoaded", function (event) {
 
     console.log("DOM fully loaded and parsed");
-    button = document.getElementById("myBtn");
-    console.log(button.attributes);
-    
-    console.log(Object.getOwnPropertyNames(new human("Jorge")));
-    console.log(Object.getOwnPropertyNames(button.attributes));
-    //Array(4) [ "name", "favoriteAnimes", "school", "age
-    //Array(6) [ "0", "1", "2", "id", "type", "class" ]
-    for (const key in button.attributes) {
-       console.log(key);
-    }
+    document.getElementById("myBtn").addEventListener("click", () => {
+        let secondColumn = document.getElementById("secondColumn");
+        let valueOfInput = document.getElementById("inputNumericField").value;
+        let result = checkValue(valueOfInput);
+        if (!result) { prompt("Add a numeric value"); }
+        else {
+            let myMatriz = new Matriz(valueOfInput, 100, -100);
+            myMatriz.fillArray();
+            let oneDimensionTable = myMatriz.getOneDimensionalHtmlTable();
+            secondColumn.removeChild(secondColumn.childNodes[0]);
+            secondColumn.appendChild(oneDimensionTable);
+
+        }
+    });
+
 });
 
-
-//for of prints the values.
-//forin prints the keys on an object
-
-class human {
-    constructor(name) {
-        this.name = name;
-        this.favoriteAnimes = ["hxh", "naruto"];
-        this.school = "Tec uruapan";
-        this.age = 25;
-    }
-    setName(name) {
-        this.name = name;
-    }
+function checkValue(value) {
+    return Number.isInteger(parseInt(value)) && value > 0;
 }
+
+class Matriz {
+    constructor(elementOfArray, positiveLimit, negativeLimit) {
+        this.__elementOfArray = parseInt(elementOfArray);
+        this.__positiveLimit = positiveLimit;
+        this.__negativeLimit = negativeLimit;
+        this.matriz = [];
+    }
+
+    fillArray() {
+
+        for (let index = 0; index < this.__elementOfArray; index++) {
+            if (Math.random() > 0.5) {
+                const randomNumber = Math.floor(Math.random() * this.__negativeLimit);
+                this.matriz[index] = randomNumber;
+            } else {
+                const randomNumber = Math.floor(Math.random() * this.__positiveLimit);
+                this.matriz[index] = randomNumber;
+            }
+        }
+
+    }
+    printArray() {
+        console.log(this.matriz);
+    }
+
+    getArray() {
+        return this.matriz;
+    }
+    getOneDimensionalHtmlTable() {
+        let table = document.createElement("table");
+        let tr = document.createElement("tr");
+        for (const value of this.matriz) {
+            tr.appendChild
+            let td = document.createElement("td");
+            let textNode = document.createTextNode(value);
+            td.appendChild(textNode);
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+        return table;
+    }
+    getTwoDimensionalHtmlTable() { }
+}
+
+
